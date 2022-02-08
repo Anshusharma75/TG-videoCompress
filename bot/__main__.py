5b579487f274a59755c2eb7bd6149d2db7ec6bbe
Anshu kumar <97522397+Anshusharma75@users.noreply.github.com>
2022-02-08 12:30:44 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,6 @@
 #    This file is part of the CompressorQueue distribution.
 #    Copyright (c) 2021 Danish_00
-#    Script Improved by Zylern
-
+#    Script Improved by Anshusharma
 
 from . import *
 from .config import *
